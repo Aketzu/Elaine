9c19f12424dc5872f58590dc4bba7fd3808f76cd
Mikael Lavi <mikael.lavi@gmail.com>
2006-08-01 13:14:03 +0000

    test
    git-svn-id: https://svn.nodeta.fi/elaine2006/trunk@87 01425017-5318-0410-addf-8e808ea31f8f

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,3 +18,13 @@ config.action_view.debug_rjs                         = true
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+# Configure the ActionMailer
+ActionMailer::Base.server_settings = {
+  :address => "mail.assembly.org",
+  :port => 25,
+  :domain => "mail.assembly.org",
+  :user_name => "mikael.lavi@assemblytv.net",
+  :password => "",
+  :authentication => :login
+}
