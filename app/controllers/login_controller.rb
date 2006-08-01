e3f6edbcd098bd43d567b410d291f60f2fd91206
Mikael Lavi <mikael.lavi@gmail.com>
2006-08-01 11:07:17 +0000

    Correct behaviour for the Login example. Need to convert to INT before evaluation, propably just some other number format.
    git-svn-id: https://svn.nodeta.fi/elaine2006/trunk@79 01425017-5318-0410-addf-8e808ea31f8f

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,17 +1,16 @@
 class LoginController < ApplicationController
-
-def index
 require 'net/http'
 require 'net/https'
 
-http = Net::HTTP.new('intra.assembly.org', 443)
-http.use_ssl = true
-path = '/cgi/livecrew/iglue/login.pl?u=late&p=test'
-
-# GET request
-resp, data = http.get(path, nil)
-
-@value = data
+def index
+  http = Net::HTTP.new('intra.assembly.org', 443)
+  http.use_ssl = true
+  path = '/cgi/livecrew/iglue/login.pl?u=Kjue&p=mcx2798'
+  
+  # GET request
+  resp, data = http.get(path, nil)
+  
+  @value = (data.to_i == 1)
 end
 
 end
