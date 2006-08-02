aede1382b7117c567db07aa0de33c2496267da53
Mikael Lavi <mikael.lavi@gmail.com>
2006-08-02 16:52:16 +0000

    git-svn-id: https://svn.nodeta.fi/elaine2006/trunk@124 01425017-5318-0410-addf-8e808ea31f8f

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,10 +13,10 @@
 set :application, "elaine2006"
 set :deploy_to, "/data/httpd/asmtv/deploy/elaine2006"
 set :repository, "https://svn.nodeta.fi/elaine2006/trunk"
-# set :user, "mlavi"
+set :user, "mlavi"
 # set :svn, "svn+ssh://moukari.assembly.org/data/livesvn/elaine2006"
-# ssh_options[:keys] = %w(/home/mlavi/.ssh/id_dsa)
-# ssh_options[:port] = 25
+ssh_options[:keys] = %w(/home/mlavi/.ssh/id_dsa)
+ssh_options[:port] = 25
 set :use_sudo, false
 
 # role :web, "intra.assembly.org/livecrew/elaine"
