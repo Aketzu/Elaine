891d719c56837468f4e75dbf58080d6bd7117a9d
Mikael Lavi <mikael.lavi@gmail.com>
2006-08-02 08:34:17 +0000

    -better configuration for deployment
    git-svn-id: https://svn.nodeta.fi/elaine2006/trunk@103 01425017-5318-0410-addf-8e808ea31f8f

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,8 @@ ActionController::Routing::Routes.draw do |map|
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
-  map.connect ':controller/service.wsdl', :action => 'wsdl'
+  map.connect 'elaine/:controller/service.wsdl', :action => 'wsdl'
 
   # Install the default route as the lowest priority.
-  map.connect ':controller/:action/:id'
+  map.connect 'elaine/:controller/:action/:id'
 end
