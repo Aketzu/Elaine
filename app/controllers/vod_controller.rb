d03a53b5fc324c1741fd86e071c183325f810426
Lauri Kangas <lauri.c.kangas@iki.fi>
2006-08-06 09:08:26 +0000

    fix for vod select
    git-svn-id: https://svn.nodeta.fi/elaine2006/trunk@220 01425017-5318-0410-addf-8e808ea31f8f

--- app/controllers/vod_controller.rb
+++ app/controllers/vod_controller.rb
@@ -4,7 +4,7 @@ class VodController < ApplicationController
   before_filter :require_no_ssl if (RAILS_ENV == "production")
   
   def next  
-        program_ids = Program.find_by_sql "SELECT programs.id as program_id, MIN(vod_format_id) as vod_format_id FROM programs JOIN vod_groups ON programs.vod_group_id = vod_groups.id JOIN vod_group_format_links ON vod_groups.id = vod_group_format_links.vod_group_id WHERE vod_format_id NOT IN (SELECT vod_format_id FROM programs JOIN vods ON programs.id = vods.program_id) AND programs.do_vod = true GROUP BY program_id, programs.vod_group_id ORDER BY programs.vod_group_id DESC;"
+        program_ids = Program.find_by_sql "SELECT p1.id as program_id, MIN(vod_format_id) as vod_format_id FROM programs p1 LEFT JOIN vod_groups ON p1.vod_group_id = vod_groups.id JOIN vod_group_format_links ON vod_groups.id = vod_group_format_links.vod_group_id WHERE vod_format_id NOT IN (SELECT vod_format_id FROM programs p2 JOIN vods ON p1.id = vods.program_id AND p2.id = p1.id) AND p1.do_vod = true GROUP BY program_id, p1.vod_group_id ORDER BY p1.vod_group_id DESC;"
         for results in program_ids 
           program = Program.find(results[:program_id])
           if program.file_exists? or RAILS_ENV == "development" # TODO: dev environment should have working file locations?
@@ -17,7 +17,6 @@ class VodController < ApplicationController
               redirect_to :action => 'error', :message => 'File location "' + program.VodGroup.name + '" missing'
               return
             end
-            Vod.transaction do
               @vod = Vod.new(:filename => program.filename + '_' + @vod_format.vcodec + '_' + @vod_format.vbitrate.to_s + 'kbps',
                              :file_location_id => vod_location.id,
                              :vod_format_id => @vod_format.id,
@@ -47,7 +46,7 @@ class VodController < ApplicationController
               else
                 redirect_to :action => 'error', :message => 'could not save vod'
                 return
-              end
+
             end
           end
         end
