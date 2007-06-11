203f6869bfce8b3652a62ae7d16f1d17366072fb
Lauri Kangas <lauri.c.kangas@iki.fi>
2007-06-11 19:41:53 +0000

    removed the menu engine in favour of Lavi's new menu system
    git-svn-id: https://svn.nodeta.fi/elaine2006/trunk@278 01425017-5318-0410-addf-8e808ea31f8f

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,21 +1,14 @@
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-#require 'login_engine'
-
-
 
 class ApplicationController < ActionController::Base
+
 #sets up acts as authenticated
-include AuthenticatedSystem
+  include AuthenticatedSystem
+
   helper SimpleSidebarHelper
   include SimpleSidebar
-#  include LoginEngine
-#  include UserEngine
 
-#  helper :user
-#  model :user
-
-#  before_filter :authorize_action
 before_filter :login_required
 
   def require_ssl
