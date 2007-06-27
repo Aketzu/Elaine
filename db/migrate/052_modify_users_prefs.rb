00eaa5415a9e91c651292fbb971461a48266c8b1
Anssi Kolehmainen <anssi.kolehmainen@iki.fi>
2007-06-27 16:04:06 +0000

    Fixed migration 52 if no Asm07 was in db
    git-svn-id: https://svn.nodeta.fi/elaine2006/trunk@308 01425017-5318-0410-addf-8e808ea31f8f

--- db/migrate/052_modify_users_prefs.rb
+++ db/migrate/052_modify_users_prefs.rb
@@ -2,7 +2,19 @@ class ModifyUsersPrefs < ActiveRecord::Migration
   def self.up
     add_column :users, :content_filter_date, :date, :default => "2007-05-31"
     add_column :users, :language, :string
-    add_column :users, :channel_id, :string, :default => Channel.find(:first, :conditions => [ "name = ?",'AssemblyTV 2007' ]).id
+		
+		chan = Channel.find_by_name("AssemblyTV 2007")
+		unless chan 
+			asm07 = Channel.new
+			asm07.name="AssemblyTV 2007"
+			asm07.description = asm07.name
+			asm07.save!
+			chan = asm07
+		end
+
+		
+
+    add_column :users, :channel_id, :string, :default => chan.id
     # For existing users we use the following code to set default values
     users = User.find_all
     for u in users
