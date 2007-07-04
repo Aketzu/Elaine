c08668b8ea5f83827eb7a72effb208da6f6a64da
Lauri Kangas <lauri.c.kangas@iki.fi>
2007-07-04 18:41:10 +0000

    These should be the 2007 preproduction db migrations.
    git-svn-id: https://svn.nodeta.fi/elaine2006/trunk@334 01425017-5318-0410-addf-8e808ea31f8f

--- db/migrate/054_modify_vod_groups.rb
+++
@@ -1,7 +0,0 @@
-class ModifyVodGroups < ActiveRecord::Migration
-  def self.up
-  end
-
-  def self.down
-  end
-end
--- db/migrate/058_modify_file_locations.rb
+++ db/migrate/058_modify_file_locations.rb
@@ -1,7 +1,11 @@
 class ModifyFileLocations < ActiveRecord::Migration
   def self.up
+    add_column :file_locations, :use_for_vods, :string
+    add_column :file_locations, :use_for_production, :string
   end
 
   def self.down
+    remove_column :file_locations, :use_for_vods, :string
+    remove_column :file_locations, :use_for_production, :string
   end
 end
