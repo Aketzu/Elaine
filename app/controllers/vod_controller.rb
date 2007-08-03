a0644271af025fb8c8f524da3c1ce84fb3455238
Anssi Kolehmainen <anssi.kolehmainen@iki.fi>
2007-08-03 15:59:31 +0000

    Honor do_vod flag
    git-svn-id: https://svn.nodeta.fi/elaine2006/trunk@424 01425017-5318-0410-addf-8e808ea31f8f

--- app/controllers/vod_controller.rb
+++ app/controllers/vod_controller.rb
@@ -10,7 +10,7 @@ class VodController < ApplicationController
 			INNER JOIN program_categories pc ON p.program_category_id = pc.id
 			INNER JOIN vod_groups vg ON pc.vod_group_id = vg.id
 			INNER JOIN vod_group_format_links vgf ON vgf.vod_group_id = vg.id
-			WHERE p.file_exists AND p.status_id = 3
+			WHERE p.file_exists AND p.status_id = 3 AND p.do_vod
 			
 			EXCEPT
 
