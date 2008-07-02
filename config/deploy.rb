b98a5dcb2a91bbc9aedfacb17ab38a02a083828b
Anssi Kolehmainen <anssi.kolehmainen@iki.fi>
2008-07-02 19:38:06 +0000

    Added passenger recipe
    git-svn-id: https://svn.nodeta.fi/elaine2006/trunk@446 01425017-5318-0410-addf-8e808ea31f8f

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,3 +14,15 @@ set :repository,  "https://svn.nodeta.fi/elaine2006/trunk"
 role :app, "elaine.aketzu.net"
 role :web, "elaine.aketzu.net"
 role :db,  "elaine.aketzu.net", :primary => true
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
