f4ae4494e845bf7ef7efc33b15c5b77900cde7e6
Anssi Kolehmainen <anssi.kolehmainen@iki.fi>
2008-07-26 20:47:10 +0000

    Really fixed the playlist now
    git-svn-id: https://svn.nodeta.fi/elaine2006/trunk@466 01425017-5318-0410-addf-8e808ea31f8f

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -5,8 +5,6 @@ class Playlist < ActiveRecord::Base
 	named_scope :for_channel, lambda { |ch| { :conditions => { :channel_id => ch } } }
 
 	def end_time
-		ret ||= start_at + program.length
-		ret ||= start_at
-		ret
+		ret = start_at + program.length
 	end
 end
