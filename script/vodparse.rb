cf4059df375d69f66fb166791abc6562852509c7
Anssi Kolehmainen <anssi.kolehmainen@iki.fi>
2008-07-29 17:07:43 +0000

    Updated vodparse script to fix program filenames
    git-svn-id: https://svn.nodeta.fi/elaine2006/trunk@483 01425017-5318-0410-addf-8e808ea31f8f

--- script/vodparse.rb
+++ script/vodparse.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
-require File.dirname(__FILE__) + '/config/boot'
-require File.dirname(__FILE__) + '/config/environment'
+require File.dirname(__FILE__) + '/../config/boot'
+require File.dirname(__FILE__) + '/../config/environment'
 
 #Parse vod filenames from moukari (find-style output) and update DB
 
-File.open("vods.txt","r") { |f|
+File.open("script/vods.txt","r") { |f|
 	f.each { |r|
 		r.chomp!
 		p = r.split "/"
@@ -15,9 +15,19 @@ File.open("vods.txt","r") { |f|
 		fn.gsub! /^[0-9]*_/, ""
 		puts r + " - " + fn
 
-		v = Vod.find_by_filename(fn)
-		next if v.nil?
-		v.full_path = "http://media.assembly.org/" + r
-		v.save!
+		if fn.include? "preview"
+			fn.gsub! /_preview/, ""
+			v = Vod.find(:first, :conditions => ["filename LIKE ?", fn+"%"])
+			next if v.nil?
+			p = v.program
+			puts p.id.to_s + ": " + p.filename + " - " + fn
+			p.filename = fn
+			p.save!
+		else
+			v = Vod.find_by_filename(fn)
+			next if v.nil?
+			v.full_path = "http://media.assembly.org/" + r
+			v.save!
+		end
 	}
 }
