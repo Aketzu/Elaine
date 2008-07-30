850b08e392178da5c915c7448426d6faef3c8765
Mikael Lavi <mikael.lavi@gmail.com>
2008-07-30 12:48:58 +0000

    Fixed playlist length usage.
    git-svn-id: https://svn.nodeta.fi/elaine2006/trunk@486 01425017-5318-0410-addf-8e808ea31f8f

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,14 +1,14 @@
 class Playlist < ActiveRecord::Base
-	belongs_to :channel
-	belongs_to :program
+    belongs_to :channel
+    belongs_to :program
 
-	validates_associated :channel, :program
-	validates_presence_of :channel, :program
-	
-	named_scope :for_channel, lambda { |ch| { :conditions => { :channel_id => ch } } }
+    validates_associated :channel, :program
+    validates_presence_of :channel, :program
 
-	def end_time
-		return 0 if program.nil?
-		ret = start_at + program.total_length
-	end
+    named_scope :for_channel, lambda { |ch| { :conditions => { :channel_id => ch } } }
+
+    def end_time
+        return 0 if program.nil?
+        ret = start_at + program.target_length
+    end
 end
